18f382ad625893b94f188942e3979985d654a9fa
stallberg <stallber@abo.fi>
2018-10-17 18:09:39 +0300

    web version

---
+++ server.py
@@ -0,0 +1,95 @@
+from flask import Flask, request, render_template
+
+# for getting the samplerate
+import soundfile as sf
+
+#The required google client libraries
+from google.cloud import storage
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
+
+app = Flask(__name__)
+
+#Initialize the speech client for making api requests
+speech_client = speech.SpeechClient()
+
+def generate_speech_config(sample_rate):
+    speech_config = types.RecognitionConfig(
+    encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+    sample_rate_hertz = sample_rate,
+    language_code = 'en-US')
+
+    return speech_config
+
+#Synchronous speech rec for audio < 1 min, otherwise async speech recognition
+def recognize(data, sample_rate, audio_duration_seconds):
+
+    audio = types.RecognitionAudio(content=data)
+    speech_config = generate_speech_config(sample_rate)
+
+    #Synchronous speech recognition
+    if audio_duration_seconds < 60:
+        response = speech_client.recognize(speech_config, audio)
+
+    #Async speech recognition
+    #TODO make it work with cloud storage
+    else:
+        return 'Cannnot send inline audio that is longer than 1 minute.'
+        #response = speech_client.long_running_recognize(speech_config, audio)
+    
+    #for debugging
+    print("Response:\n\n", response)
+
+    transcription = ''
+    for result in response.results:
+        transcription += result.alternatives[0].transcript
+
+    return transcription
+
+
+
+#TODO IMPLEMENT THIS TO STORE AUDIO FILE ON CLOUD STORAGE WHEN LENGTH > 1 MINUTE
+def upload_to_bucket(data):
+  
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket('speech_recognition_audio')
+
+    #blob = bucket.blob('test')
+    #blob.upload_from_file()
+    
+    #blob = bucket.blob(blob_name)
+    #blob.upload_from_filename(path_to_file)
+
+    #returns a public url
+    #return blob.public_url
+
+
+@app.route('/')
+def mainpage():
+    return render_template("index.html")
+
+@app.route('/recognize', methods=['POST'])
+def audio():
+
+    #get the file from the post from client
+    data = request.files['audio_data']
+
+    # get sample rate and audio duration
+    f = sf.SoundFile(data)
+    sample_rate = f.samplerate
+    audio_duration_seconds = (len(f) / sample_rate)
+
+    #Debugging
+    print(sample_rate)
+    print(audio_duration_seconds)
+
+    response = recognize(data.read(), sample_rate, audio_duration_seconds)   
+    return response
+
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', debug=True)
+
+
---
+++ static/js/app.js
@@ -0,0 +1,81 @@
+var microphoneStream;
+var recorder;
+var input;
+ 
+var AudioContext = window.AudioContext || window.webkitAudioContext;
+var audioContext = new AudioContext;
+ 
+var recordButton = document.getElementById("recordButton");
+var transcriptionOutput = document.getElementById("transcription")
+
+//Alternate between start and stop recording for same recordButton
+var startStopRecording = (function(){
+    var start = true;
+    return function() {
+        start ? startRecording() : stopRecording();
+        start = !start;
+    }
+})();
+
+recordButton.addEventListener("click", startStopRecording);
+
+
+function startRecording() {
+
+    recordButton.innerHTML = "Stop Recording";
+
+    //returns a promise
+    navigator.mediaDevices.getUserMedia({ audio: true, video: false})
+    
+    .then(function(stream) {
+        //store stream inside another variable to get access to it outside of startRecording()
+        microphoneStream = stream;
+        input = audioContext.createMediaStreamSource(stream);
+ 
+        /* Create the recorder (Record.js library)
+        Google cloud speech requires mono audio */
+        recorder = new Recorder(input, { numChannels: 1} )
+        recorder.record()
+ 
+    }).catch(function(error) {
+        recordButton.innerHTML = "Record"
+        console.log(error);
+    });
+}
+
+
+function stopRecording() {
+    //Disable button until response from server
+    recordButton.disabled = true;
+    recorder.stop();
+ 
+    //stop the microphone stream
+    microphoneStream.getAudioTracks()[0].stop();
+    
+    //create blob and pass to uploadToServer function
+    recorder.exportWAV(uploadToServer);
+}
+
+function uploadToServer(blob) {
+
+	var filename = "test"
+    
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function(event) {
+        if(xhr.readyState === 4){
+            //display response on screen
+            transcriptionOutput.innerHTML = event.target.responseText;
+        }
+        else {
+            console.log("Error");
+            transcriptionOutput.innerHTML = "error";  
+        }
+        recordButton.disabled = false;
+        recordButton.innerHTML = "Record";
+    };
+
+    var formdata = new FormData();
+    formdata.append("audio_data",blob, filename);
+    xhr.open("POST","http://35.228.65.146:5000/recognize",true);
+    xhr.send(formdata);
+}
