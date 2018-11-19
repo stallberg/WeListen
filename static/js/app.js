e03b5c895949b5c830544863ac3dfeac870edef9
PepeTheDeveloper <petri.linjaaho@gmail.com>
2018-11-19 19:50:46 +0200

    Proof of concept implementations

--- static/js/app.js
+++ static/js/app.js
@@ -9,8 +9,10 @@ let recordButton = document.getElementById("recordButton");
 let saveButton = document.getElementById("saveButton");
 let previousButton = document.getElementById("previousButton");
 let nextButton = document.getElementById("nextButton");
+let reviewButton = document.getElementById("reviewButton");
 let transcriptionOutput = document.getElementById("transcription");
 let questionOutput = document.getElementById("question");
+let questionHeader = document.getElementById("questionHeader");
 
 //Alternate between start and stop recording for same recordButton
 let startStopRecording = (function(){
@@ -21,7 +23,13 @@ let startStopRecording = (function(){
     }
 })();
 
-// Function to be implemented
+// Function saves answers as sessionstorage items
+let updateQuestionHeader = function(){
+    let questionNumber = ind+1;
+    questionHeader.innerHTML = "<b>Question " + questionNumber + ":</b>";
+};
+
+// Function saves answers as sessionstorage items
 let saveAnswer = function(){
     if (transcriptionOutput){
         let answer = transcriptionOutput.innerHTML;
@@ -29,40 +37,68 @@ let saveAnswer = function(){
     }
 };
 
-// Function to be implemented
+// previous function (needs refactoring)
 let previousQuestion = function(){
     ind--;
     if (ind !== 0){
+        updateQuestionHeader();
         transcriptionOutput.innerHTML = sessionStorage.getItem('answerKey' + ind);
         questionOutput.innerHTML = questions[ind];
         nextButton.disabled = false;
     }
     else{
+        updateQuestionHeader();
         transcriptionOutput.innerHTML = sessionStorage.getItem('answerKey' + 0);
         questionOutput.innerHTML = questions[ind];
         previousButton.disabled = true;
     }
 };
 
-// Function to be implemented
+// next function (needs refactoring)
 let nextQuestion = function(){
     ind++;
     if (ind < questions.length){
+        updateQuestionHeader();
         transcriptionOutput.innerHTML = sessionStorage.getItem('answerKey' + ind);
         questionOutput.innerHTML = questions[ind];
         previousButton.disabled = false;
     }
     else{
         //transcriptionOutput.innerHTML = sessionStorage.getItem('answerKey' + ind);
-        questionOutput.innerHTML = "You reached the final question";
+        questionHeader.innerHTML = "<b>You reached the final question</b>";
+        questionOutput.innerHTML = "Please review the form before submitting";
         nextButton.disabled = true;
+        reviewButton.style.visibility = "visible";
     }
+};
+
+// review form
+let reviewForm = function(){
+    let i;
+    let reviewOutput = "";
+    for (i = 0; i < questions.length; i++) {
+        if (sessionStorage.getItem('answerKey' + i) == null) {
+            reviewOutput = reviewOutput + "Question " + (i+1) + "<br>" + questions[i] + "<br>" + "<b>Not answered!</b>" + "<br>";
+        }
+        else {
+            reviewOutput = reviewOutput + "Question " + (i+1) + "<br>" + questions[i] + "<br>" + sessionStorage.getItem('answerKey' + i) + "<br>";
+        }
+    }
+
+    var w = window.open("");
+    w.document.write("<html><head><title>Form Review</title></head><body><b>Vehicle accident claim form:</b></body></html>");
+    p = document.createElement("p");
+    p.innerHTML = reviewOutput;
+    w.document.body.appendChild(p);
+
+    //document.getElementById("demo").innerHTML = reviewOutput;
 };
 
 recordButton.addEventListener("click", startStopRecording);
 saveButton.addEventListener("click", saveAnswer);
 previousButton.addEventListener("click", previousQuestion);
 nextButton.addEventListener("click", nextQuestion);
+reviewButton.addEventListener("click", reviewForm);
 
 // initialize buttons as disabled
 //saveButton.disabled = true;
@@ -70,6 +106,9 @@ previousButton.disabled = true;
 
 //question counter
 let ind = 0;
+
+//initialize question header on page load
+updateQuestionHeader();
 
 // View stored answer on page load if any
 transcriptionOutput.innerHTML = sessionStorage.getItem('answerKey' + ind);
