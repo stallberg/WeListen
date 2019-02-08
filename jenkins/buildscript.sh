a373a179f97d8c476abda1c68803771d81b4db31
stallberg <stallber@abo.fi>
2019-02-08 19:11:16 +0200

    Updated docker-compose yml

--- jenkins/buildscript.sh
+++ jenkins/buildscript.sh
@@ -1,4 +1,3 @@
 # Simple shell script executed as a jenkins buildstep
-
 docker-compose down
 docker-compose up -d --build
--- jenkins/run.sh
+++ jenkins/run.sh
@@ -1,4 +1,4 @@
-docker run -d --name jenkins \
+docker run -d --name jenkins --restart always \
 -v /var/run/docker.sock:/var/run/docker.sock \
 -v $(which docker):$(which docker) -v $(which docker-compose):$(which docker-compose) \
 -v jenkins_home:/var/jenkins_home --network="welisten_welisten" jenkins:welisten
