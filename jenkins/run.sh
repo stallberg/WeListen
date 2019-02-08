fa2a05002b52d676ba14fc45689c9a8e66296079
stallberg <stallber@abo.fi>
2019-02-08 19:22:09 +0200

    Updated run script

--- jenkins/run.sh
+++ jenkins/run.sh
@@ -1,4 +1,4 @@
 docker run -d --name jenkins --restart always \
 -v /var/run/docker.sock:/var/run/docker.sock \
--v $(which docker):$(which docker) -v $(which docker-compose):$(which docker-compose) \
+-v /usr/bin/docker:/usr/bin/docker -v /usr/local/bin/docker-compose:/usr/local/bin/docker-compose \
 -v jenkins_home:/var/jenkins_home --network="welisten_welisten" jenkins:welisten
