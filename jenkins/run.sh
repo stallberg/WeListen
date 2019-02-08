64d7158269770fd4930c328454ffbacf464d0fee
stallberg <stallber@abo.fi>
2019-02-08 19:26:08 +0200

    Updated run script for jenkins

--- jenkins/run.sh
+++ jenkins/run.sh
@@ -1,4 +1,1 @@
-docker run -d --name jenkins --restart always \
--v /var/run/docker.sock:/var/run/docker.sock \
--v /usr/bin/docker:/usr/bin/docker -v /usr/local/bin/docker-compose:/usr/local/bin/docker-compose \
--v jenkins_home:/var/jenkins_home --network="welisten_welisten" jenkins:welisten
+docker run -d --name jenkins --restart always -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker -v /usr/local/bin/docker-compose:/usr/local/bin/docker-compose -v jenkins_home:/var/jenkins_home --network="welisten_welisten" jenkins:welisten
