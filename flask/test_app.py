17529ead1f4878de14d07b5e9292b174d405e70b
stallberg <stallber@abo.fi>
2019-03-11 12:34:03 +0200

    Added some initial unit tests with the pytest library. Testing the different routes and functions etc.

---
+++ flask/test_app.py
@@ -0,0 +1,57 @@
+from server import app
+from databaseApi import DBApi
+from text_to_speech import tts
+import json
+import pytest
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    client = app.test_client()
+    yield client
+
+#Index page displaying forms
+def test_index_page(client):
+    rv = client.get('/')
+    status_code = rv.status_code
+    assert status_code == 200
+    response = rv.data
+    assert b'<title>We Listen - Forms</title>' in response
+
+def test_car_insurance_form(client):
+    rv = client.get('/forms/1/')
+    status_code = rv.status_code
+    assert status_code == 200
+    response = rv.data
+    assert b'<h1 id="form-title">Car Insurance Claim</h1>' in response
+
+def test_bug_report_form(client):
+    rv = client.get('/forms/2/')
+    status_code = rv.status_code
+    assert status_code == 200
+    response = rv.data
+    assert b'<h1 id="form-title">Bug Report</h1>' in response
+
+def test_km_perdiem_form(client):
+    rv = client.get('/forms/3/')
+    status_code = rv.status_code
+    assert status_code == 200
+    response = rv.data
+    assert b'<h1 id="form-title">Requesting Kilometre and Per Diem Allowance </h1>' in response
+
+def test_car_insurance_json(client):
+    rv = client.get('/forms/1/json/')
+    json_data = json.loads(rv.data)
+    assert len(json_data.keys()) == 4
+    assert json_data['id'] == 1
+    assert len(json_data['questions']) == 4
+    print(json_data.keys())
+
+
+def test_DBapi_get_forms(client):
+    dbApi = DBApi()
+    forms = dbApi.getForms()
+    assert len(forms) == 3
+
+
+    
